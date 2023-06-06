c97dba5b37481696dbaa681927c01136c23ad486
jx-ke <q220187@selby.ac.uk>
2023-06-06 11:51:27 +0100

    Level 2 connection

--- JakesUnityGame/Assets/Finish.cs
+++ JakesUnityGame/Assets/Finish.cs
@@ -1,25 +1,2 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Finish : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.name == "Player")
-        {
-            CompleteLevel();
-        }
-    }
-    // Update is called once per frame
-    private void CompleteLevel()
-    {
-        
-    }
-}
+   
