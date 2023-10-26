12cbeea29a6c5492d7372a598ed3198818fc6786
Gitjxk3 <q220187@selby.ac.uk>
2023-10-26 16:25:25 +0100

    Timer working

---
+++ JakesUnityGame/Assets/ScoreManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText;
+
+    public int score = 0;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        scoreText.text = score.ToString() + "/3 collected";
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        scoreText.text = score.ToString() + "/3 collected";
+        Debug.Log(score);
+    }
+}
