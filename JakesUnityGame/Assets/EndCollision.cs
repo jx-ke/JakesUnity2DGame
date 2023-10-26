b27630d892ea9a73646add2985dde2339506e0d2
Gitjxk3 <q220187@selby.ac.uk>
2023-10-26 21:03:24 +0100

    Second Level DONE and weather added

---
+++ JakesUnityGame/Assets/EndCollision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class EndCollisionDONTDELETE : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (scoreManager != null)
+        {
+            
+            Debug.Log(scoreManager);
+            if (scoreManager.score == 3 && collision.gameObject.tag == "End") //two conditions need to be met: score = 3 and collision with object with "End" tag
+            {
+                
+                SceneManager.LoadScene(3); // loads the End screen
+                
+            }
+        }
+    }
+
+   
+    
+}
