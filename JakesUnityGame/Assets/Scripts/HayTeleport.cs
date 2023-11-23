9c19ddb13d5ed98d57c562308e675f4218ca08b0
Gitjxk3 <q220187@selby.ac.uk>
2023-11-23 14:42:39 +0000

    Secret level DONE

---
+++ JakesUnityGame/Assets/Scripts/HayTeleport.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class HayTeleport : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            
+            if (collision.gameObject.tag == "Secret") //two conditions need to be met: score = 3 and collision with object with "End" tag
+            {
+                
+                SceneManager.LoadScene(5); // loads the End screen
+                
+            }
+        }
+    }
+
+   
+    
+}
