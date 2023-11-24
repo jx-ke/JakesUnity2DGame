62089892a3578eca46eee07a0122b5537c0ea0a2
Gitjxk3 <q220187@selby.ac.uk>
2023-11-24 10:29:24 +0000

    Secret level REDESIGN

--- JakesUnityGame/Assets/Scripts/StartLevel.cs
+++ JakesUnityGame/Assets/Scripts/StartLevel.cs
@@ -1,17 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections;
+using System.IO;
+using System;
+using System.Threading;
 using UnityEngine.UI;
-
+ 
 public class StartLevel : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+ 
+    static bool startGame=false;
+    public Text txt;
+   
+    // Use this for initialization
+ 
+    void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        txt = gameObject.GetComponent<Text>();
+        }
+    void Start () {
+   
+        startGame = true;
+           
+        if (startGame == true)
         {
-        SceneManager.LoadScene(3);
+        StartCoroutine("CountDown");
         }
     }
-}
+ 
+        IEnumerator CountDown()
+{
+ 
+        yield return new WaitForSeconds(1);
+        txt.text= "3";
+        yield return new WaitForSeconds(1);  
+        txt.text= "2";
+        yield return new WaitForSeconds(1);
+        txt.text=  "1";
+        yield return new WaitForSeconds(1);
+        txt.text= "GO!";
+        yield return new WaitForSeconds(1);
+        txt.text= "";
+//
+    }
+       }
